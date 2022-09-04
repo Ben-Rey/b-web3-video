9dfb58c73d833de06006fccaa4f06c62dac67d2f
fen|x <benjamin.reynes.dev@gmail.com>
2022-09-04 17:20:24 +0200

    setup

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+};
+
+export default config;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+  },
+};
